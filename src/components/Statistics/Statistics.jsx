e944e6c0e65f0e5a9b43c9d8a6d1a29f4be35c70
YevhenTsilenko <fiboss1992@gmail.com>
2021-09-16 08:57:34 +0300

    bug fixes

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -18,7 +18,7 @@ Statistics.propTypes = {
     neutral: PropTypes.number.isRequired,
     bad: PropTypes.number.isRequired,
     total: PropTypes.number.isRequired,
-    positivePercentage: PropTypes.number.isRequired,
+    positiveFeedbacks: PropTypes.number.isRequired,
   };
 
 export { Statistics };
